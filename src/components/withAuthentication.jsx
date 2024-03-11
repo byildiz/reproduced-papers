6549e2f75ed6d9ecb4de1195021a1e2cafc08939
Burak Yildiz <0byldz@gmail.com>
2024-03-11 14:59:01 +0100

    fix spinner

--- src/components/withAuthentication.jsx
+++ src/components/withAuthentication.jsx
@@ -1,5 +1,5 @@
-import { Spinner } from 'react-bootstrap'
 import { useAuthentication } from '../hooks'
+import Spinner from './Spinner'
 import ErrorAlert from './ErrorAlert'
 
 export default function withAuthentication(Component) {
